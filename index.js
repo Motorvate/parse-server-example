53ddf05c1958e12df1352076fb06585932c84264
Lin Zhou <zhoulin0522@gmail.com>
2017-02-11 22:27:12 -0500

    Add push notification certificate to server.

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+	push: {
+		ios: {
+			pfx: 'certificates/PushCertificates.p12', // the path and filename to the .p12 file you exported earlier. 
+			bundleId: 'com.motorvate.ios', // The bundle identifier associated with your app
+			production: false // Specifies which environment to connect to: Production (if true) or Sandbox
+		}
+	}
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
