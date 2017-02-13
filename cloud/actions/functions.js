a2aaa42c84cdf9b2c784779296b45830151de88b
Lin Zhou <zhoulin0522@gmail.com>
2017-02-13 01:00:04 -0500

    half way to set up API endpoint to posting comments to events.

--- cloud/actions/functions.js
+++ cloud/actions/functions.js
@@ -2,3 +2,10 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('createEventComment', function(req, res) {
+	var eventID = req.params.eventID;
+	var authorID = req.params.authorID;
+	var parentCommentID = req.params.parentCommentID;
+	var comment = req.params.comment;
+});
