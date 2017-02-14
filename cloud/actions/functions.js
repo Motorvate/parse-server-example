3accd6291707dd008c19352b6d6734041bc0578c
Lin Zhou <zhoulin0522@gmail.com>
2017-02-13 23:55:40 -0500

    adding an API endpoint for saving new event comment.

--- cloud/actions/functions.js
+++ cloud/actions/functions.js
@@ -4,8 +4,38 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define('createEventComment', function(req, res) {
-	var eventID = req.params.eventID;
-	var authorID = req.params.authorID;
+	var queryPromises = [];
+
+	var eventQuery = new Parse.Query("Event");
+	var eventPromise = eventQuery.get(req.params.eventID);
+	queryPromises.push(eventPromise);
+
+	var userQuery = new Parse.Query("User");
+	var userPromise = userQuery.get(req.params.authorID);
+	queryPromises.push(userPromise);
+
 	var parentCommentID = req.params.parentCommentID;
-	var comment = req.params.comment;
+	if (parentCommentID != null) {
+		var commentQuery = new Parse.Query("EventComment");
+		var commentPromise = commentQuery.get(parentCommentID);
+		queryPromises.push(commentPromise);		
+	}
+
+	Parse.Promise.when(queryPromises).then(function(results) {
+		var EventComment = Parse.Object.extend("EventComment");
+		var newComment = new EventComment();
+		newComment.comment = req.params.comment;
+		newComment.set("event", results[0]);
+		newComment.set("author", results[1]);
+		if (results.length == 3) {
+			newComment.set("parentComment", results[2]);
+		}
+		return newComment.save();
+	}, function(errors){
+		res.error(errors);
+	}).then(function(saveResult){
+		res.success(saveResult);
+	}, function(saveError){
+		res.error(saveError);
+	});
 });
