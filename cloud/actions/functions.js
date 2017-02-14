aa6afa8d0f3e279ff2d0dbbab6741b3ffc511819
Lin Zhou <zhoulin0522@gmail.com>
2017-02-14 00:20:19 -0500

    Trying to fix an issue that user’s comment string is missing in event comment API. Syntax issue spotted.

--- cloud/actions/functions.js
+++ cloud/actions/functions.js
@@ -4,11 +4,8 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define('createEventComment', function(req, res) {
-	console.log("user comment 1: " + req.params.comment);
-
+	var commentString = req.params.comment;
 	var queryPromises = [];
-	var stringPromise = Parse.Promise.as(req.params.comment);
-	queryPromises.push(stringPromise);
 
 	var eventQuery = new Parse.Query("Event");
 	var eventPromise = eventQuery.get(req.params.eventID);
@@ -26,15 +23,13 @@ Parse.Cloud.define('createEventComment', function(req, res) {
 	}
 
 	Parse.Promise.when(queryPromises).then(function(results) {
-	console.log("user comment 2: " + results[0]);
-
 		var EventComment = Parse.Object.extend("EventComment");
 		var newComment = new EventComment();
-		newComment.comment = results[0];
-		newComment.set("event", results[1]);
-		newComment.set("author", results[2]);
-		if (results.length == 4) {
-			newComment.set("parentComment", results[3]);
+		newComment.set("comment", commentString);
+		newComment.set("event", results[0]);
+		newComment.set("author", results[1]);
+		if (results.length == 3) {
+			newComment.set("parentComment", results[2]);
 		}
 		return newComment.save();
 	}, function(errors){
