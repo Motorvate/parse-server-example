241bd336e05f1f1dccb5bc0191463d61c14636ec
Lin Zhou <zhoulin0522@gmail.com>
2017-02-18 23:55:16 -0500

    Added an API endpoint for posting shop review.

--- cloud/actions/functions.js
+++ cloud/actions/functions.js
@@ -33,8 +33,8 @@ Parse.Cloud.define('createEventComment', function(req, res) {
 			newComment.set("parentComment", results[2]);
 		}
 		return newComment.save();
-	}, function(errors) {
-		res.error(errors);
+	}, function(queryError) {
+		res.error(queryError);
 	}).then(function(savedComment) {
 		outerComment = savedComment;
 		return savedComment.get("event").fetch();
@@ -47,12 +47,64 @@ Parse.Cloud.define('createEventComment', function(req, res) {
 						data: { alert: "A new comment from a RSVPed event: " + event.get("name") }
 					}, { useMasterKey: true }
 		);
-	}, function(error){
-			res.error(error);
-	})
-	.then(function(){
+	}, function(eventFetchError){
+		res.error(eventFetchError);
+	}).then(function(){
 		res.success(outerComment);
 	}, function(pushError){
 		res.error(pushError);
 	});
 });
+
+Parse.Cloud.define('createShopReview', function(req, res) {
+	var queryPromises = [];
+
+	var shopQuery = new Parse.Query("Shop");
+	var shopPromise = shopQuery.get(req.params.shopID);
+	queryPromises.push(shopPromise);
+
+	var userQuery = new Parse.Query("User");
+	var userPromise = userQuery.get(req.params.authorID);
+	queryPromises.push(userPromise);
+
+	var parentReviewID = req.params.parentReviewID;
+	if (parentReviewID != null) {
+		var parentReviewQuery = new Parse.Query("ShopReview");
+		var parentReviewPromise = parentReviewQuery.get(parentReviewID);
+		queryPromises.push(parentReviewPromise);		
+	}
+
+	var outerReview = null;
+	Parse.Promise.when(queryPromises).then(function(results){
+		var ShopReview = Parse.Object.extend("ShopReview");
+		var newReview = new ShopReview();
+		newReview.set("shop", results[0]);
+		newReview.set("author", results[1]);
+		newReview.set("review", req.params.review);
+		newReview.set("reviewScore", req.params.score);
+		if (parentReviewID != null) {
+			newReview.set("parentReview", results[2]);
+		}
+		return newReview.save();
+	}, function(queryError){
+		res.error(queryError);
+	}).then(function(savedReview){
+		outerReview = savedReview;
+		return savedReview.get("shop").fetch();
+	}, function(saveError){
+		res.error(saveError);
+	}).then(function(shop){
+		var notificationMessage = "A new review of your favorite shop: " + shop.get("name")
+		return Parse.Push.send({
+						channels: [shop.id],
+						data: { alert: notificationMessage }
+					}, { useMasterKey: true }
+		);
+	}, function(fetchShopError){
+		res.error(fetchShopError);
+	}).then(function(){
+		res.success(outerReview);
+	}, function(pushError){
+		res.error(pushError);
+	});
+}
