fc7897336562c4d9afe68a763e20962a009dd5d6
Lin Zhou <zhoulin0522@gmail.com>
2017-02-24 14:35:59 -0500

    Update shop’s review score when a new review is posted.

--- cloud/actions/functions.js
+++ cloud/actions/functions.js
@@ -254,9 +254,24 @@ Parse.Cloud.define('createShopReview', function(req, res) {
 	}, function(saveError){
 		res.error(saveError);
 	}).then(function(){
-		res.success(outerReview);
+		var reviewQuery = new Parse.Query("ShopReview");
+		reviewQuery.equalTo("shop", outerShop);
+		return reviewQuery.find();
 	}, function(pushError){
 		res.error(pushError);
+	}).then(function(queryResults){
+		var totalScore = 0.0;
+		for (var i = 0; i < queryResults.length; i++) {
+			totalScore = totalScore + queryResults[0].get("reviewScore");
+		}
+		outerShop.set("reviewScore", totalScore / queryResults.length);
+		return outerShop.save();
+	}, function(reviewQueryError){
+		res.error(reviewQueryError);
+	}).then(function(savedShop){
+		res.success(outerReview);
+	}, function(shopSaveError){
+		res.error(shopSaveError);
 	});
 });
 
