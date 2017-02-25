6693fc456a65f7ba7fbc8b48da5c490fe6d08332
Lin Zhou <zhoulin0522@gmail.com>
2017-02-24 22:25:06 -0500

    fixing shop review query.

--- cloud/actions/functions.js
+++ cloud/actions/functions.js
@@ -255,7 +255,7 @@ Parse.Cloud.define('createShopReview', function(req, res) {
 		res.error(saveError);
 	}).then(function(){
 		var reviewQuery = new Parse.Query("ShopReview");
-		reviewQuery.equalTo("shop.id", outerShop.id);
+		reviewQuery.equalTo("shop", outerShop);
 		return reviewQuery.find();
 	}, function(pushError){
 		res.error(pushError);
@@ -267,7 +267,7 @@ Parse.Cloud.define('createShopReview', function(req, res) {
 		outerShop.set("reviewScore", totalScore / queryResults.length);
 		return outerShop.save();
 	}, function(reviewQueryError){
-		res.error(reviewQueryError);
+		res.error({reviewQueryError: "error!!!"});//reviewQueryError
 	}).then(function(savedShop){
 		res.success(outerReview);
 	}, function(shopSaveError){
